7d493be82819d4251ba039ba481d79f284c93bfa
akersj <jamie@jamieakers.co.uk>
2016-04-15 11:44:16 +0100

    Initial commit

---
+++ SpotifyController/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SpotifyController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.whatsPlaying = new System.Windows.Forms.Timer(this.components);
+            this.scrollingText = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(22, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.label1.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Webdings", 8.25F);
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(0, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.label2.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Webdings", 8.25F);
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(536, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "r";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            // 
+            // whatsPlaying
+            // 
+            this.whatsPlaying.Enabled = true;
+            this.whatsPlaying.Interval = 1000;
+            this.whatsPlaying.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scrollingText
+            // 
+            this.scrollingText.Enabled = true;
+            this.scrollingText.Interval = 50;
+            this.scrollingText.Tick += new System.EventHandler(this.scrollingText_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(554, 21);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.6D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpotifyController";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer whatsPlaying;
+        private System.Windows.Forms.Timer scrollingText;
+    }
+}
+
---
+++ SpotifyController/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpotifyController
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", EntryPoint = "GetWindowText", CharSet = CharSet.Auto)]
+        static extern IntPtr GetWindowCaption(IntPtr hwnd, StringBuilder lpString, int maxCount);
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = 21;
+            this.Top = 0;
+        }
+        private void Skip()
+        {
+            IntPtr hwnd = FindWindow("SpotifyMainWindow", null);
+
+            PostMessage(hwnd, 0x319, IntPtr.Zero, new IntPtr(0xB0000L));
+        }
+        private string GetPlaying()
+        {
+            IntPtr hwnd = FindWindow("SpotifyMainWindow", null);
+            StringBuilder sb = new StringBuilder(256);
+            GetWindowCaption(hwnd, sb, 256);
+            return sb.ToString();
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                int WM_NCLBUTTONDOWN = 0xA1;
+                int HT_CAPTION = 0x2;
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Skip();
+        }
+
+        private bool trackchanged = false;
+        private int waitsincechange = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = GetPlaying();
+
+            if (previoustext != GetPlaying())
+            {
+                //track changed
+                label1.Left = 22;
+                trackchanged = true;
+                scrollingText.Enabled = false;
+            }
+
+            if (trackchanged)
+            {
+                waitsincechange++;
+
+                if (waitsincechange > 3)
+                {
+                    waitsincechange = 0;
+                    trackchanged = false;
+                    scrollingText.Enabled = true;
+                }
+            }
+            
+
+            previoustext = GetPlaying();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Opacity = .6;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string previoustext = "";
+        private void scrollingText_Tick(object sender, EventArgs e)
+        {
+
+            if (label1.Text == "Spotify")
+            {
+                label1.Left = 22;
+            }
+            else
+            {
+                label1.Left = label1.Left - 1;
+                if (label1.Left <= 0 - label1.Width)
+                {
+                    label1.Left = this.Width;
+                }
+
+            }
+        }
+    }
+
+}
